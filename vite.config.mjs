becc0ba0ded13592ff4d596f588887fa75a99acc
“LamentXU123” <108666168+LamentXU123@users.noreply.github.com>
2026-05-28 22:55:48 +0800

    add checks doc

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,9 +13,11 @@ export default defineConfig({
       input: {
         main: resolve(rootDir, 'doc/index.html'),
         'docs-index': resolve(rootDir, 'doc/docs/index.html'),
+        'docs-checks': resolve(rootDir, 'doc/docs/checks.html'),
         'docs-parameters': resolve(rootDir, 'doc/docs/parameters.html'),
         'zh-cn': resolve(rootDir, 'doc/zh-cn/index.html'),
         'zh-cn-docs-index': resolve(rootDir, 'doc/zh-cn/docs/index.html'),
+        'zh-cn-docs-checks': resolve(rootDir, 'doc/zh-cn/docs/checks.html'),
         'zh-cn-docs-parameters': resolve(rootDir, 'doc/zh-cn/docs/parameters.html')
       }
     }
